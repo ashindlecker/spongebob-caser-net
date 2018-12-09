c486e026f8c5fe5504dc19a2e2329716dba297da
Austin Shindlecker <ashindlecker@gmail.com>
2018-12-09 00:16:50 -0700

    Passing of first test

--- cli/SpongeCaser.cs
+++ cli/SpongeCaser.cs
@@ -4,6 +4,26 @@ namespace SpongeCase
     {
         public static string ConvertToSpongeCase(string input)
         {
+            //Counter to keep track of each normal letter [A-Z][a-z] that was found
+            var normalCharacterCounter = 0;
+
+            var convertedStringToBuild = "";
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var currentCharacter = input[i];
+
+                if(currentCharacter != ' ')
+                    normalCharacterCounter++;
+
+                if(normalCharacterCounter % 2 == 0){
+                    convertedStringToBuild += input[i].ToString().ToUpper();
+                }
+                else {
+                    convertedStringToBuild += input[i].ToString().ToLower();
+                }
+            }
+            return convertedStringToBuild;
         }
     }
 }
