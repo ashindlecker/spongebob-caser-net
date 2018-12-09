1b6efa64da62bfbeaf911d934c53ef8b2495f537
Austin Shindlecker <ashindlecker@gmail.com>
2018-12-09 00:33:08 -0700

    More tests, along with an optional "Start with capital" parameter

--- cli/SpongeCaser.cs
+++ cli/SpongeCaser.cs
@@ -4,9 +4,9 @@ namespace SpongeCase
 {
     public class SpongeCaser
     {
-        public static string ConvertToSpongeCase(string input)
+        public static string ConvertToSpongeCase(string input, bool startWithCapitalLetter = false)
         {
-            var capitilizeCharacterFlag = true;
+            var capitilizeCharacterFlag = !startWithCapitalLetter;
 
             var convertedStringToBuild = "";
 
--- tests/SpongeCaser.cs
+++ tests/SpongeCaser.cs
@@ -9,13 +9,16 @@ namespace tests
         [Fact]
         public void SpongeCaseTransformTest()
         {
-            Assert.Equal("hElLo WoRlD", SpongeCaser.ConvertToSpongeCase("Hello World"));
             Assert.Equal("hElLoWoRlD", SpongeCaser.ConvertToSpongeCase("HelloWorld"));
+            Assert.Equal("hElLoWoRlD", SpongeCaser.ConvertToSpongeCase("helloWorld"));
             Assert.Equal(" hElLo WoRlD ", SpongeCaser.ConvertToSpongeCase(" Hello World "));
 
             //More Special Characters Test
             Assert.Equal("[hElLo WoRlD]", SpongeCaser.ConvertToSpongeCase("[Hello World]"));
             Assert.Equal("====bBbB====", SpongeCaser.ConvertToSpongeCase("====bbbb===="));
+
+            //Test of starting with a capital letter by using an optional parameter
+            Assert.Equal("HeLlO wOrLd", SpongeCaser.ConvertToSpongeCase("Hello World", true));
         }
     }
 }
